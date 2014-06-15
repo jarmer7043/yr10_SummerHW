3075be429eba758a32897c1f3b25e42747c60962
jarmer7043 <jarmer7043@sohamcollege.org.uk>
2014-06-15 11:32:37 +0100

    Times Table
    Does all the tasks

--- HW/2/tables.py
+++ HW/2/tables.py
@@ -1,1 +1,13 @@
-#paste code here
+import time #To use in the printing
+
+table = 0 #Itterating the variable table
+
+while table > 100 or table < 1: #Loop infinitely until a sensible number is entered
+    table = int(input("Enter a times table >>>"))
+
+number = int(input("Up to what number >>>")) #To what number the multiplications go up to 
+
+for x in range(1,number + 1): #Up to number + 1 so it will go to the desired number
+    print("{0} X {1} = {2}".format(x,table,table*x)) #Prints the number, the user's number it is multiplying by and the answer
+    time.sleep(1) #Aesthetics
+
